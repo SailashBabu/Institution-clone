e7f1d9588b36dc94a5e977148b0320fd8b519365
yuvaraj-e03 <yuvarajeswaran76@gmail.com>
2025-03-10 16:16:49 +0530

    Assignment page initial

--- src/components/assignment.jsx
+++ src/components/assignment.jsx
@@ -10,6 +10,27 @@ export default function Assignments(){
             <button>PENDING</button>
             <button>ALL</button>
         </div>
+        <div className="selection-bar">
+        <input type="date"></input>
+        <input type="date"></input>
+        <select name="Subjects">
+            <option>Subjects</option>
+            <option>FullStack</option>
+            <option>Devops</option>
+        </select>
+        </div>
+       
+        <table>
+            <div className="table-header">
+            <tr>
+                <th>DUE DATE</th>
+                <th>PROJECT</th>
+                <th>TYPE</th>
+                <th>STATUS</th>
+            </tr>
+            </div>
+        </table>
+
         </>
     )
 }
